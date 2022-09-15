9a1bdadd3c5e2aae4148955b666daa383fa4af71
Pavithran Baskaran <37231548+PavithranBaskaran@users.noreply.github.com>
2022-09-15 18:01:02 +0530

    Update api.js

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3009",
+  baseURL: "https://redux-post-backend.vercel.app",
 });
 
 export default instance;
